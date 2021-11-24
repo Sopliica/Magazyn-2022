9e25583d828a0cf8c73d0db85fdb535fd8af53b6
Jacek Soplica <fg058201@student.ath.edu.pl>
2021-11-24 19:16:08 +0100

    Abstrakcyjny user     Pola     Deklaracja fukcji GetNewId

---
+++ Magazyn 2022/AUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magazyn_2022
+{
+    public abstract class AUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public bool[] AccesLevel { get; set; }
+
+        public int GetNewId()
+        {
+            return 0;
+        }
+    }
+}
