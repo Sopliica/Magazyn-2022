e453c444b5aad8e03ef92ab0ae6acffc909bdc72
UrwiDzyndzel <mencelkamil@gmail.com>
2022-02-06 18:17:49 +0100

    Zrobilem wedlug schematu FactoryMethodCurier i factoryMEthod Domesticcurier

---
+++ Magazyn 2022/Controler/Factory Method Curier/DomesticCurierCreator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Magazyn_2022.Model.Curiers;
+
+namespace Magazyn_2022.Controler.Factory_Method_Curier
+{
+    public class DomesticCurierCreator : CurierCreator
+    {
+        public override ICurier Create(string name, string surname, string licenceNumber, int accesLvl)
+        {
+            DomesticCurier curier = new DomesticCurier();
+            
+            curier.Id = CurrentFreeId;
+            curier.Name = name;
+            curier.Surname = surname;
+            curier.LicenceNumber = licenceNumber;
+            curier.AccesLvL = accesLvl;
+            CurrentFreeId++;
+            return curier;
+        }
+    }
+}
