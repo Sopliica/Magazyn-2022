c7b244e879e3e87d56ac39e971ff99f4dc675baa
UrwiDzyndzel <mencelkamil@gmail.com>
2022-02-06 18:01:26 +0100

    model kurier, klasa abstrakcyjna z funkcjonalnosciami, interfej i zwykly kurier (klasa)

---
+++ Magazyn 2022/Controler/Factory Method Curier/ICurier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Magazyn_2022.Model;
+
+namespace Magazyn_2022.Controler.Factory_Method_Curier
+{
+    public interface ICurier
+    {
+        List<AOrder> GetOrderList();
+        List<APack> GetPackList();
+        void OrderReturned(AOrder order);
+    }
+}
